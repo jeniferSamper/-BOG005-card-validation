59ce2c8e26561629c705455a37478926f243b798
samper <jenifersamperusb@gmail.com>
2022-07-10 11:15:28 -0500

    10/07/22

--- src/validator.js
+++ src/validator.js
@@ -4,6 +4,14 @@ const validator = {
 
 export default validator;
 
-export function algoritmo (){
-  alert('si exporto')
+//funcion guardar en array 
+export function algoritmo (creditCardNumber){
+  var casillas = new Array();
+  for (var i= 0 ; i < creditCardNumber.length ; i = i+1 ){
+  casillas[i] = creditCardNumber.charAt(i);
+  }
+  console.log('casillas', casillas)
+  casillas.reverse();
+  console.log('inverto ', casillas);
+ alert('Invertido ' + casillas)
 }
