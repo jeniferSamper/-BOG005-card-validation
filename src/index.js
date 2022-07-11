02b40b5185bdf7ed731e36c91d0929961678adf3
jeniferSamper <jenifersamperusb@gmail.com>
2022-07-11 09:41:21 -0500

    prueba

--- src/index.js
+++ src/index.js
@@ -3,12 +3,11 @@ import validator,{algoritmo } from './validator.js';
 console.log(validator);
 
 var boton = document.getElementById('guardar')
-var tarjeta = document.getElementById('tc')
+var creditCardNumber = document.getElementById('tc')
 
 
 boton.onclick = function guardar(){
-    tarjeta = tarjeta.value;
-    
-    alert('el numero es' + tarjeta);
-    algoritmo();
+    creditCardNumber = creditCardNumber.value;
+    alert('el numero es' + creditCardNumber);
+    algoritmo(creditCardNumber);
 }
--- src/validator.js
+++ src/validator.js
@@ -7,12 +7,13 @@ export default validator;
 //funcion guardar en array 
 export function algoritmo (creditCardNumber){
   var casillas = new Array();
-  for (var i= 0 ; i < creditCardNumber.length ; i = i+1 ){
+  alert('corre algoritmo')
+  console.log(typeof creditCardNumber)
+  for (var i = 0; i<creditCardNumber.length; i = i+1 ){
   casillas[i] = creditCardNumber.charAt(i);
   }
   console.log('casillas', casillas)
   casillas.reverse();
   console.log('inverto ', casillas);
- alert('Invertido ' + casillas)
- alert('probando github escritotio')
+  alert('Invertido ' + casillas);
 }
