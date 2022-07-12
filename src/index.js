4192c8de594ff56770b66187b68d537efacfec04
jeniferSamper <jenifersamperusb@gmail.com>
2022-07-12 15:13:39 -0500

    validacion de datos y pruebas lista

--- src/index.js
+++ src/index.js
@@ -4,11 +4,31 @@ import validator from './validator.js';
 
 var boton = document.getElementById('guardar');
 var creditCardNumber = document.getElementById('tc');
+var nombreTitular = document.getElementById('nombretitular');
+var cvv = document.getElementById('cvv');
+var fechav = document.getElementById('fechav');
+var vacio = document.getElementById('vacio');
 
 
 boton.onclick = function guardar(){
-    creditCardNumber = creditCardNumber.value;
+    //creditCardNumber = creditCardNumber.value;
     //console.log('el numero de TC es ' + creditCardNumber);
-    validator.isValid(creditCardNumber);
-    validator.maskify(creditCardNumber);}
+    
+    if (nombreTitular.value == null || nombreTitular.value == ''){
+        vacio.innerHTML = ('Ingresa nombre de titular');
+    } else if(creditCardNumber.value == null || creditCardNumber.value == ''){
+        vacio.innerHTML = ('Ingresa un número de tarjeta');
+    } else if (cvv.value == null || cvv.value == ''){
+        vacio.innerHTML =('Ingresa codigo CVV');
+    } else if (fechav.value == null || fechav.value == ''){
+        vacio.innerHTML =('Ingresa fecha de vencimiento');
+    } else{
+        creditCardNumber = creditCardNumber.value;
+        vacio.innerHTML = ('')
+        validator.isValid(creditCardNumber);
+        validator.maskify(creditCardNumber);
+
+    }
+}
+
     
