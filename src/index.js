3c098363170db44ab84df95b02d07ee13f6441ac
jeniferSamper <jenifersamperusb@gmail.com>
2022-07-13 14:47:54 -0500

    cambio en los for

--- src/index.js
+++ src/index.js
@@ -7,31 +7,33 @@ var creditCardNumber = document.getElementById('tc');
 var nombreTitular = document.getElementById('nombretitular');
 var cvv = document.getElementById('cvv');
 var fechav = document.getElementById('fechav');
-var vacio = document.getElementById('vacio');
+var mensaje = document.getElementById('mensaje');
 var resultado1;
+var resultado2;
+
 boton.onclick = function guardar(){
     //creditCardNumber = creditCardNumber.value;
     //console.log('el numero de TC es ' + creditCardNumber);
     
     if (nombreTitular.value == null || nombreTitular.value == ''){
-        vacio.innerHTML = ('Ingresa nombre de titular');
+        mensaje.innerHTML = ('Ingresa nombre de titular');
     } else if(creditCardNumber.value == null || creditCardNumber.value == ''){
-        vacio.innerHTML = ('Ingresa un número de tarjeta');
+        mensaje.innerHTML = ('Ingresa un número de tarjeta');
     } else if (cvv.value == null || cvv.value == ''){
-        vacio.innerHTML =('Ingresa codigo CVV');
+        mensaje.innerHTML =('Ingresa codigo CVV');
     } else if (fechav.value == null || fechav.value == ''){
-        vacio.innerHTML =('Ingresa fecha de vencimiento');
+        mensaje.innerHTML =('Ingresa fecha de vencimiento');
     } else{
         creditCardNumber = creditCardNumber.value;
-        vacio.innerHTML = ('')
+        mensaje.innerHTML = ('')
         validator.isValid(creditCardNumber);
-        var resultado2 = validator.maskify(creditCardNumber);
+        resultado2 = validator.maskify(creditCardNumber);
         if(validator.isValid(creditCardNumber) == true){
             resultado1 = 'valida'
         } else {
             resultado1 = 'invalida'
         }
-        vacio.innerHTML =(' Su tarjeta ' + resultado2 + ' es ' + resultado1);
+        mensaje.innerHTML =(' Su tarjeta ' + resultado2 + ' es ' + resultado1);
         
         
 
--- src/validator.js
+++ src/validator.js
@@ -5,13 +5,11 @@ const validator = {
     var suma = 0;
     var total;
     var valido;
-    //guarda el numero en un array y lo invierte.
-    //console.log(typeof creditCardNumber)
-    casillas = creditCardNumber.split("");
-    //console.log('casillas', casillas)
-    casillas.reverse();
-    //console.log('inverto ', casillas);
     
+    //guarda el numero en un array y lo invierte.
+    casillas = creditCardNumber.split("");
+    casillas.reverse();
+  
     //se multiplica por dos los pares y suma los dos digitos si el numero es mayor a 9.
     for(var i = 1; i< casillas.length ; i +=2){
       if(casillas[i]<5){
@@ -19,26 +17,18 @@ const validator = {
       }
       else{
         casillas[i]= ((casillas[i] - 5)* 2) + 1;
-      }
-      }
-      //console.log('array despues de multiplicar',casillas);
-      for(var j = 0; j< casillas.length; j++){
-      suma += Number(casillas[j]);
-      }
-      //console.log('suma', suma)
+      }}
+      casillas.forEach(function(a){suma+=Number(a)})
       
-      // calculamos si es valido o no
+      //calculamos si es valido o no y retorna un booleano.
       total = suma % 10;
-     //console.log('si es cero es valido: ', total)
       valido = total == 0;
-     //console.log(' la TC es validA?', valido)
-     return valido;
+      return valido;
     },
 
-  maskify: function (creditCardNumber){
+    maskify: function (creditCardNumber){
     var simbolo = '#'; 
     var mascara = (creditCardNumber.slice(0, -4).replace(/./g, simbolo) + (''+ creditCardNumber.slice(-4)));
-    //console.log('dato mascara', mascara);
     return mascara;
   }
   };
