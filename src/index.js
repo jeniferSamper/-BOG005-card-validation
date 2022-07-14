1528bb93f281b9e4e6405675da691d58b2b54e15
jeniferSamper <jenifersamperusb@gmail.com>
2022-07-14 10:03:12 -0500

    cambios en funcionalidad

--- src/index.js
+++ src/index.js
@@ -1,19 +1,35 @@
 import validator from './validator.js';
 
 //console.log(validator);
+var opc=document.getElementById('opc');
+
+//esta funcion muestra las opciones a elegir.
+opc.onclick = function MostrarOpciones (){
+    var opc1=document.getElementById('opc1');
+    var opc2=document.getElementById('opc2');
+    var selecciona=document.getElementById('selecciona');
+    document.getElementById('pantalla1').style.display ='none';
+    if (opc1.checked == true ){
+        document.getElementById('pantalla2opc1').style.display = 'block';
+        document.getElementById('pantalla2').style.display = 'block';
+    } else if( opc2.checked == true){
+        document.getElementById('pantalla2opc2').style.display = 'block';
+        document.getElementById('pantalla2').style.display = 'block';
+    } else {
+        document.getElementById('pantalla1').style.display ='block';
+        selecciona.innerHTML= ('Selecciona una opción');
+    }
+}
 
 var boton = document.getElementById('guardar');
-var creditCardNumber = document.getElementById('tc');
-var nombreTitular = document.getElementById('nombretitular');
-var cvv = document.getElementById('cvv');
-var fechav = document.getElementById('fechav');
-var mensaje = document.getElementById('mensaje');
-var resultado1;
-var resultado2;
-
 boton.onclick = function guardar(){
-    //creditCardNumber = creditCardNumber.value;
-    //console.log('el numero de TC es ' + creditCardNumber);
+    var creditCardNumber = document.getElementById('tc');
+    var nombreTitular = document.getElementById('nombretitular');
+    var cvv = document.getElementById('cvv');
+    var fechav = document.getElementById('fechav');
+    var mensaje = document.getElementById('mensaje');
+    var resultado1;
+    var resultado2;
     
     if (nombreTitular.value == null || nombreTitular.value == ''){
         mensaje.innerHTML = ('Ingresa nombre de titular');
@@ -38,6 +54,19 @@ boton.onclick = function guardar(){
         
     }
     return false;
+}
+
+var regresar = document.getElementById('regresar');
+regresar.onclick = function (){
+    document.getElementById('pantalla1').style.display = 'block';
+}
+
+var reservar= document.getElementById('reservar');
+reservar.onclick= function (){
+    document.getElementById('pantalla2opc1').style.display = 'none';
+    document.getElementById('pantalla2opc2').style.display = 'none';
+    document.getElementById('pantalla2').style.display= 'none';
+    document.getElementById('pantalla3').style.display = 'block';
 }
 
     
